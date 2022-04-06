a47beff58dc3c8778f96d76854c6c27f26a9295e
koba <kenichiro.kobayashi@stockmark.co.jp>
2022-04-07 01:19:06 +0900

    並列処理の道立

--- object_verb_structureing.py
+++ object_verb_structureing.py
@@ -2,6 +2,7 @@ import spacy
 from spacy.symbols import obj
 from chunker import ChunkExtractor
 from subject_get import SubjectExtractor
+from parallel_get import ParallelExtractor
 
 class VerbExtractor:
 
@@ -15,60 +16,8 @@ class VerbExtractor:
         self.verb_chunk = chunker.verb_chunk
         subj_get = SubjectExtractor()
         self.subject_get = subj_get.subject_get_from_object
-
-    """
-        並列句の取得
-        objやsubjの格がついていないがかかり先が同じものは並立句とみなす
-    """
-    def para_get(self, start, end, *doc):
-        ret = []
-        sp = start
-        ep = end
-        find_ct = 0
-        for i in reversed(range(0, start)):
-            if sp <= i:
-                continue
-            if ((doc[i].pos_ == 'NOUN' or doc[i].pos_ == 'PROPN') and doc[i].tag_ != '名詞-普通名詞-副詞可能' and (i >= ep or i < sp) or
-                ((doc[i].head.head.i >= ep or doc[i].head.head.i < sp) and doc[doc[i].head.i].norm_ == '他')):
-                if len(doc) > i + 1 and doc[i + 1].lemma_ == 'の' and doc[i + 1].pos_ == 'ADP':  # 〇〇の〇〇　は並列扱いしない
-                    if (doc[i].head.i >= sp and doc[i].head.i <= ep):
-                        if not find_ct:
-                            ret.append(('', 0, 0))
-                        return ret
-                    else:
-                        continue
-#                if token.tag_ == '名詞-普通名詞-サ変可能' and (doc[token.i + 1].pos_ == 'PUNCT' or doc[token.i + 1].pos_ == 'SYM'):  # サ変名詞、〇〇　は並列扱いしない
-#                    return '', 0, 0
-                if (doc[i].head.i >= sp and doc[i].head.i <= ep) and (doc[i + 1].lemma_ == 'と' or doc[i + 1].lemma_ == 'や' or doc[i + 1].lemma_ == 'など' or doc[i + 1].pos_ == 'PUNCT'):
-                    ret.append((self.num_chunk(i, *doc)))
-                    find_ct = find_ct + 1
-                    sp = ret[find_ct - 1][1]
-                    ep = ret[find_ct - 1][2]
-                    continue
-                if (doc[i + 1].pos_ == 'ADP' and doc[i + 1].lemma_ != 'など'):
-                    break
-                if (doc[i].head.head.i >= ep or doc[i].head.head.i < sp) and (doc[i].pos_ == 'NOUN' or doc[i].pos_ == 'PROPN') and\
-                        (doc[i + 1].lemma_ == '、' or doc[doc[i].head.i].i == i + 1) and doc[doc[i].head.i].norm_ == '他' and doc[doc[i].head.i + 1].pos_ != 'ADP':
-                    ret.append((self.num_chunk(i, *doc)))
-                    find_ct = find_ct + 1
-                    sp = ret[find_ct - 1][1]
-                    ep = ret[find_ct - 1][2]
-                    continue
-                if(doc[i].head.i == doc[ep].head.i and (doc[i].tag_ != '名詞-普通名詞-サ変可能' or doc[i + 1].tag_ != '補助記号-読点')):
-                    ret.append((self.num_chunk(i, *doc)))
-                    find_ct = find_ct + 1
-                    sp = ret[find_ct - 1][1]
-                    ep = ret[find_ct - 1][2]
-                    continue
-
-        if not find_ct:
-            for i in reversed(range(0, doc[end].head.i)):
-                if i != end:
-                    if doc[i].head.i == doc[end].head.i and doc[i].lemma_ == '共同':
-                        ret = self.para_get(i, i, *doc)
-                        return ret
-            ret.append(('', 0, 0))
-        return ret
+        parallel_get = ParallelExtractor()
+        self.para_get = parallel_get.para_get
 
 
     """
---
+++ parallel_get.py
@@ -0,0 +1,67 @@
+from chunker import ChunkExtractor
+
+class ParallelExtractor:
+
+    def __init__(self):
+        """
+        関数`__init__`はクラスをインスタンス化した時に実行されます。
+        """
+        chunker = ChunkExtractor()
+        self.num_chunk = chunker.num_chunk
+
+
+    """
+        並列句の取得
+        objやsubjの格がついていないがかかり先が同じものは並立句とみなす
+    """
+    def para_get(self, start, end, *doc):
+        ret = []
+        sp = start
+        ep = end
+        find_ct = 0
+        for i in reversed(range(0, start)):
+            if sp <= i:
+                continue
+            if ((doc[i].pos_ == 'NOUN' or doc[i].pos_ == 'PROPN') and doc[i].tag_ != '名詞-普通名詞-副詞可能' and (i >= ep or i < sp) or
+                ((doc[i].head.head.i >= ep or doc[i].head.head.i < sp) and doc[doc[i].head.i].norm_ == '他')):
+                if len(doc) > i + 1 and doc[i + 1].lemma_ == 'の' and doc[i + 1].pos_ == 'ADP':  # 〇〇の〇〇　は並列扱いしない
+                    if (doc[i].head.i >= sp and doc[i].head.i <= ep):
+                        if not find_ct:
+                            ret.append(('', 0, 0))
+                        return ret
+                    else:
+                        continue
+#                if token.tag_ == '名詞-普通名詞-サ変可能' and (doc[token.i + 1].pos_ == 'PUNCT' or doc[token.i + 1].pos_ == 'SYM'):  # サ変名詞、〇〇　は並列扱いしない
+#                    return '', 0, 0
+                if (doc[i].head.i >= sp and doc[i].head.i <= ep) and (doc[i + 1].lemma_ == 'と' or doc[i + 1].lemma_ == 'や' or doc[i + 1].lemma_ == 'など' or doc[i + 1].pos_ == 'PUNCT'):
+                    ret.append((self.num_chunk(i, *doc)))
+                    find_ct = find_ct + 1
+                    sp = ret[find_ct - 1][1]
+                    ep = ret[find_ct - 1][2]
+                    continue
+                if (doc[i + 1].pos_ == 'ADP' and doc[i + 1].lemma_ != 'など'):
+                    break
+                if (doc[i].head.head.i >= ep or doc[i].head.head.i < sp) and (doc[i].pos_ == 'NOUN' or doc[i].pos_ == 'PROPN') and\
+                        (doc[i + 1].lemma_ == '、' or doc[doc[i].head.i].i == i + 1) and doc[doc[i].head.i].norm_ == '他' and doc[doc[i].head.i + 1].pos_ != 'ADP':
+                    ret.append((self.num_chunk(i, *doc)))
+                    find_ct = find_ct + 1
+                    sp = ret[find_ct - 1][1]
+                    ep = ret[find_ct - 1][2]
+                    continue
+                if(doc[i].head.i == doc[ep].head.i and (doc[i].tag_ != '名詞-普通名詞-サ変可能' or doc[i + 1].tag_ != '補助記号-読点')):
+                    ret.append((self.num_chunk(i, *doc)))
+                    find_ct = find_ct + 1
+                    sp = ret[find_ct - 1][1]
+                    ep = ret[find_ct - 1][2]
+                    continue
+
+        if not find_ct:
+            for i in reversed(range(0, doc[end].head.i)):
+                if i != end:
+                    if doc[i].head.i == doc[end].head.i and doc[i].lemma_ == '共同':
+                        ret = self.para_get(i, i, *doc)
+                        return ret
+            ret.append(('', 0, 0))
+        return ret
+
+
