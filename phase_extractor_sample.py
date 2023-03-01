9d38618341a626d65d88ef97e301594d664c3f5c
kenichiro kobayashi <kenichiro.kobayashi@stockmark.co.jp>
2023-03-01 22:41:01 +0900

    PAS単独に分離

--- phase_extractor_sample.py
+++
@@ -1,8 +0,0 @@
-from phase_extractor import PhaseExtractor
-
-p_e = PhaseExtractor()  # KeywordExtractorのクラスのインスタンス化
-
-text = '大手スーパーのベイシアは、小型店舗の「ベイシアマート」を除く全店舗で「アセロラ真鯛」の試験販売を始めた。'
-
-phase = p_e.single_phase_extract(text)  # フェーズのチェック
-print(phase)
